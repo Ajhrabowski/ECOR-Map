ac8094cb8ebe845483541808b1795a126725ff7e
Anthony Hrabowski <ajhrabowski@gmail.com>
2024-01-14 22:09:48 -0500

    Modified models/index.js file.Finished association methods and started setting up the API routes

--- models/index.js
+++ models/index.js
@@ -6,14 +6,21 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category,{
-  
-})
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 
-// Categories have many Products
+// Categories have many Products\
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{through: ProductTag });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{through: ProductTag });
 
 module.exports = {
   Product,
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,19 +6,50 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = Tag.findAll({
+      include: [{ model: ProductTag }, { model: Product }],
+    });
+    res.status(200).json(readerData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = Tag.findByPk(req.params.id, {
+      include: [{ model: ProductTag }, { model: Product }],
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  router.put('/:id', (req, res) => {
+    try {
+      const tagData = Tag.create(req.body);
+      res.status(200).json(tagData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  
 });
 
 router.delete('/:id', (req, res) => {
